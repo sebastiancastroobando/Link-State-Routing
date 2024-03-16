b610b8b74c8c276ba1aaf112c11ffac84cbeb491
Sebastian <sebastiancastroobando@gmail.com>
2024-03-16 16:30:44 -0400

    First implementation of processAttach method

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,4 @@
+## General notes
 ### Java Socket Programming
 - GetInputStream().read() is a blocking call. Meaning that the method will block until input data is available
     - Blocking operations = bad throughput. We want to develop some way to handle concurrent socket requests. 
@@ -6,3 +7,16 @@
 ### Simulation of Link State Routing Protocol
 - We need to implement network description synchronization between routers. More specifically, implement the `Router` class.
 - We need to use "Process IP" and "Process Port" to establish connection via socket. - In the "simulated network", we assign a "simulated ip address" to each router. This IP address is only used to identify the router program instrance in the simulted netowrk space, but not used to communicate via sockets. 
+
+### Resources
+- [This video](https://www.youtube.com/watch?v=gchR3DpY-8Q) was good to understand how sockets should used.
+---
+## Dev Log
+### 2024-04-16
+- First draft of the proccessAttach method in the Router class
+- **Question** : should we check if array of link is already full and tell user that no more links can be added? Also, should we check if the link is already in the array of links?
+- **Question** : Should we specifically check if we got REJECTED response in the attach method? 
+
+---
+## To-Dos
+- [ ] Find cure for cancer
--- src/main/java/socs/network/node/Router.java
+++ src/main/java/socs/network/node/Router.java
@@ -3,7 +3,11 @@ package socs.network.node;
 import socs.network.util.Configuration;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
 
 
 public class Router {
@@ -48,7 +52,80 @@ public class Router {
    * NOTE: this command should not trigger link database synchronization
    */
   private void processAttach(String processIP, short processPort, String simulatedIP) {
-    
+    // start a socket connection with the remote router
+    // send HELLO packets
+     // Start a socket connection with remote router
+     Socket socket = null;
+     InputStreamReader inputStreamReader = null;
+     OutputStreamWriter outputStreamWriter = null;
+ 
+     // To improve performance, we will use buffer to read and write. The idea is to read and write in bulk.
+     BufferedReader bufferedReader = null;
+     BufferedWriter bufferedWriter = null;
+ 
+     try {
+      // First we need to establish a connection with the remote router
+      socket = new Socket(processIP, processPort);
+      // Then we need to create a reader and writer to read and write to the socket
+      inputStreamReader = new InputStreamReader(socket.getInputStream());
+      outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+      bufferedReader = new BufferedReader(inputStreamReader);
+      bufferedWriter = new BufferedWriter(outputStreamWriter);
+ 
+      // Send the HELLO message to the remote router
+      bufferedWriter.write("HELLO\n");
+      // Flush the buffer to send the message
+      bufferedWriter.flush();
+ 
+      // Wait for the response from the remote router
+      String response = bufferedReader.readLine(); // This will block until the remote router sends a message
+ 
+      // If response is SUCCESS, then we can create a link between the routers
+      if (response.equals("SUCCESS")) {
+        // Create a link between the routers
+        RouterDescription remoteRouter = new RouterDescription();
+        remoteRouter.simulatedIPAddress = simulatedIP;
+        remoteRouter.processIPAddress = processIP;
+        remoteRouter.processPortNumber = processPort;
+
+        // Create a link between the routers
+        Link link = new Link(rd, remoteRouter);
+        // Add the link to the ports array
+        for (int i = 0; i < ports.length; i++) {
+          if (ports[i] == null) {
+            ports[i] = link;
+            break;
+          }
+        }
+      } 
+      // REJECTED : close socket connection, but we are already closing the socket in the finally block?
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+    finally {
+      // Close all the resources
+      try {
+        if (bufferedReader != null) {
+          bufferedReader.close();
+        }
+        if (bufferedWriter != null) {
+          bufferedWriter.close();
+        }
+        if (inputStreamReader != null) {
+          inputStreamReader.close();
+        }
+        if (outputStreamWriter != null) {
+          outputStreamWriter.close();
+        }
+        if (socket != null) {
+          socket.close();
+        }
+      }
+      catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
   }
 
 
@@ -58,7 +135,7 @@ public class Router {
    * The intuition is that if router2 is an unknown/anomaly router, it is always safe to reject the attached request from router2.
    */
   private void requestHandler() {
-
+    
   }
 
   /**
@@ -75,7 +152,7 @@ public class Router {
    * This command does trigger the link database synchronization
    */
   private void processConnect(String processIP, short processPort, String simulatedIP) {
-
+   
   }
 
   /**
