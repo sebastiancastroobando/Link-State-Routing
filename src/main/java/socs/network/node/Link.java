c7a5bf65d6c7bd84fb4e1444d9ccf6141ba6264e
Denis <denisuzh@gmail.com>
2024-03-30 16:58:56 -0400

    Adds Link.destroy() method
    Signed-off-by: Denis <denisuzh@gmail.com>

--- src/main/java/socs/network/node/Link.java
+++ src/main/java/socs/network/node/Link.java
@@ -2,7 +2,9 @@ package socs.network.node;
 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class Link {
 
@@ -18,5 +20,39 @@ public class Link {
     this.socket = socket;
     this.in = in;
     this.out = out;
+  }
+
+  public void destroy() {
+    this.sourceRouter = null;
+    this.targetRouter = null;
+    if (this.in != null) {
+      try {
+        this.in.close();
+      } catch (IOException e) {
+        System.out.println("Failed to in.close() in Link");
+        e.printStackTrace();
+      }
+      this.in = null;
+    }
+    if (this.out != null) {
+      try {
+        this.out.close();
+      } catch (SocketException e) {
+        System.out.println("Connection severed, in Link.out.close()");
+      } catch (IOException e) {
+        System.out.println("Failed to out.close() in Link");
+        e.printStackTrace();
+      }
+      this.out = null;
+    }
+    if (this.socket != null) {
+      try {
+        this.socket.close();
+      } catch (Exception e) {
+        System.out.println("Failed to socket.close() in Link");
+        e.printStackTrace();
+      }
+      this.socket = null;
+    }
   }
 }
